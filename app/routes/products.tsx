4aab80f876723f0647dc4a898a30debb614860be
Tomas Pozo <tomaspozogarzon@gmail.com>
2022-03-23 22:39:44 -0500

    feat(prisma): implement prisma initial schema and products list

--- app/routes/products.tsx
+++ app/routes/products.tsx
@@ -1,5 +1,29 @@
-import { Heading } from "@chakra-ui/react";
+import { Heading, Box, ListItem, UnorderedList } from "@chakra-ui/react";
+import { Product } from "@prisma/client";
+import { json, LoaderFunction, useLoaderData } from "remix";
+import { db } from "~/utils/db.server";
+
+type LoaderData = { products: Array<Product> };
+
+export const loader: LoaderFunction = async () => {
+  const data: LoaderData = {
+    products: await db.product.findMany(),
+  };
+  return json(data);
+};
 
 export default function Products() {
-  return <Heading>Comming soon</Heading>;
+  const { products } = useLoaderData<LoaderData>();
+  return (
+    <Box>
+      <Heading>Total Products: {products.length}</Heading>
+      <Box p={4}>
+        <UnorderedList>
+          {products.map((product) => {
+            return <ListItem key={product.id}>{product.name}</ListItem>;
+          })}
+        </UnorderedList>
+      </Box>
+    </Box>
+  );
 }
---
+++ prisma/seed.ts
@@ -0,0 +1,64 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+const db = new PrismaClient();
+
+async function seed() {
+  await Promise.all(
+    getProducts().map((data) => {
+      return db.product.create({ data });
+    })
+  );
+}
+
+seed();
+
+function getProducts(): Prisma.ProductCreateInput[] {
+  return [
+    {
+      name: "Bamboo Tan",
+      currency: "USD",
+      price: 199,
+      flag: "new",
+      imageUrl:
+        "https://images.unsplash.com/photo-1602024242516-fbc9d4fda4b6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80",
+      rating: 4,
+      ratingCount: 1,
+      description:
+        "With a sleek design and a captivating essence, this is a modern Classic made for every occasion.",
+    },
+    {
+      name: "Iconic Turquoise",
+      currency: "USD",
+      price: 199,
+      salePrice: 179.99,
+      flag: "on-sale",
+      imageUrl:
+        "https://images.unsplash.com/photo-1509941943102-10c232535736?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80",
+      rating: 4,
+      ratingCount: 12,
+      description:
+        "With a sleek design and a captivating essence, this is a modern Classic made for every occasion.",
+    },
+    {
+      name: "Marble Leather",
+      currency: "USD",
+      price: 199,
+      imageUrl:
+        "https://images.unsplash.com/photo-1564594985645-4427056e22e2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80",
+      rating: 4,
+      ratingCount: 12,
+      description:
+        "With a sleek design and a captivating essence, this is a modern Classic made for every occasion.",
+    },
+    {
+      name: "Silve wolf",
+      currency: "GBP",
+      price: 199,
+      imageUrl:
+        "https://images.unsplash.com/photo-1522312346375-d1a52e2b99b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=680&q=80",
+      rating: 5,
+      ratingCount: 1,
+      description:
+        "With a sleek design and a captivating essence, this is a modern Classic made for every occasion.",
+    },
+  ];
+}
