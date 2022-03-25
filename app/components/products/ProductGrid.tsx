ce60dd44b3012883fcd5241b368bfabaa5d271e6
Tomas Pozo <tomaspozogarzon@gmail.com>
2022-03-25 14:16:30 -0500

    fix(products): set columns to 4

--- app/components/products/ProductGrid.tsx
+++ app/components/products/ProductGrid.tsx
@@ -3,21 +3,11 @@ import { useMemo, Children, isValidElement } from "react";
 import Pagination from "./Pagination";
 
 export const ProductGrid = (props: SimpleGridProps) => {
-  const columns = useMemo(() => {
-    const count = Children.toArray(props.children).filter(
-      isValidElement
-    ).length;
-    return {
-      base: Math.min(2, count),
-      lg: Math.min(4, count),
-    };
-  }, [props.children]);
-
   return (
     <Flex direction="column">
       <SimpleGrid
         height={350}
-        columns={columns}
+        columns={4}
         columnGap={{ base: "4", md: "6" }}
         rowGap={{ base: "8", md: "10" }}
         {...props}
