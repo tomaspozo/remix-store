f315eae3248564133969068bd3428300c7df5d3f
Tomas Pozo <tomaspozogarzon@gmail.com>
2022-03-25 19:14:50 -0500

    feat(db): improve records in seed data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -19,7 +19,7 @@ function getProducts(): Prisma.ProductCreateInput[] {
       imageUrl:
         "https://images.unsplash.com/photo-1602024242516-fbc9d4fda4b6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80",
       rating: 4,
-      ratingCount: 1,
+      ratingCount: 12,
       description:
         "With a sleek design and a captivating essence, this is a modern Classic made for every occasion.",
     },
@@ -43,7 +43,7 @@ function getProducts(): Prisma.ProductCreateInput[] {
       imageUrl:
         "https://images.unsplash.com/photo-1564594985645-4427056e22e2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80",
       rating: 4,
-      ratingCount: 12,
+      ratingCount: 110,
       description:
         "With a sleek design and a captivating essence, this is a modern Classic made for every occasion.",
     },
@@ -54,7 +54,31 @@ function getProducts(): Prisma.ProductCreateInput[] {
       imageUrl:
         "https://images.unsplash.com/photo-1522312346375-d1a52e2b99b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=680&q=80",
       rating: 5,
-      ratingCount: 1,
+      ratingCount: 161,
+      description:
+        "With a sleek design and a captivating essence, this is a modern Classic made for every occasion.",
+    },
+    {
+      name: "Luxury Pink",
+      currency: "USD",
+      price: 399,
+      flag: "new",
+      imageUrl:
+        "https://image.made-in-china.com/202f0j00KopUVrkqstcG/Luxury-Rose-Gold-Women-Watch-Magnet-Starry-Sky-Wrist-Watch-for-Ladies-Female-Wristwatch.webp",
+      rating: 5,
+      ratingCount: 45,
+      description:
+        "With a sleek design and a captivating essence, this is a modern Classic made for every occasion.",
+    },
+    {
+      name: "Black Leather",
+      currency: "USD",
+      price: 599,
+      flag: "new",
+      imageUrl:
+        "https://cdn.shopify.com/s/files/1/0579/4080/3782/products/1574084644869_1553839196707_611VKtzrdSL._UL1100__1_-originnm80prcnt.jpg",
+      rating: 5,
+      ratingCount: 97,
       description:
         "With a sleek design and a captivating essence, this is a modern Classic made for every occasion.",
     },
